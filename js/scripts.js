426c5d631ed86e84a7aafec4f30baf9504577e02
jessicahattig <jessicahattig@gmail.com>
2023-06-07 20:41:12 -0700

    WIP: conversions.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// business logic
+function ozTograms(number1) {
+  return number1 * 28.4
+}
+
+//user interface
+window.alert(ozTOgrams(1,28.4));
+
+
+
+
+// ounces to grams 
+// 1 oz to 28.4 grams
+
+// pounds to ounces
+// 1 lb = 453.592 oz
